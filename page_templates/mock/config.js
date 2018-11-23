bfdab8f8cbb3e672a96372f46f7c17e535885d2e
qianxuemin001 <qianxuemin001@lianjia.com>
2018-11-23 21:03:21 +0800

    feat:增加文件注释 更新模板 生成默认文件夹和文件名称

---
+++ page_templates/mock/config.js
@@ -0,0 +1,16 @@
+// 状态配置
+module.exports = {
+  'errno': 0,
+  'error': '',
+  'request_id': '2018080716183898927',
+  'data': {
+    'searchConfigList': [{
+      'id': 1,
+      'name': '选项一'
+    }, {
+      'id': 2,
+      'name': '选项二'
+    }]
+  },
+  'cost': 270
+}
---
+++ page_templates/mock/list.js
@@ -0,0 +1,37 @@
+// 列表
+module.exports = {
+  'errno': 0,
+  'error': '',
+  'request_id': '2017102516102056837',
+  'data': {
+    'total': 1,
+    'total_page': 1,
+    'current_page': 1,
+    'page_size': 20,
+    'list': [{
+      'order_code': 'LF00000001',
+      'taste_level': '五星',
+      'quantity_level': '大份',
+      'order_status_text': '就餐中',
+      'commit_time': '2018-12-01 18:00:00',
+      'order_status': 1,
+      'customer_code': '13924832222'
+    }, {
+      'order_code': 'LF00000002',
+      'taste_level': '四星',
+      'quantity_level': '中份',
+      'order_status_text': '就餐中',
+      'commit_time': '2018-12-01 18:00:00',
+      'order_status': 1,
+      'customer_code': '13924832222'
+    }, {
+      'order_code': 'LF00000003',
+      'taste_level': '三星',
+      'quantity_level': '小份',
+      'order_status_text': '就餐中',
+      'commit_time': '2018-12-01 18:00:00',
+      'order_status': 1,
+      'customer_code': '13924832222'
+    }]
+  }
+}
